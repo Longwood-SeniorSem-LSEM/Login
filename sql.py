e7e362a75ad8f77ce3f184b43595109f791c106d
Daniel Hill <dhill90@gmail.com>
2015-02-25 12:40:24 -0500

    Advanced the database setup script.
    The sql.py setup script will run the test_setup.sql file.  The
    test_setup.sql file is a minified version of the two sql files from the
    databases group.  This should allow anyone to setup the 'test' database
    fairly easily.

--- sql.py
+++ sql.py
@@ -1,32 +1,30 @@
 # sql.py - Create a SQLite3 table and populate it with data
 
 
-import sqlite3
+#import sqlite3
 
-# create a new database if the database doesn't already exist
-#with sqlite3.connect('posts.db') as connection:
-#
-#    # get a cursor object used to execute SQL commands
-#    c = connection.cursor()
-#
-#    c.execute('DROP TABLE IF EXISTS posts')
-#    # create the table
-#    c.execute('CREATE TABLE posts(title TEXT, details TEXT)')
-#
-#    # insert dummy data into the table
-#    c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
-#    c.execute('INSERT INTO posts VALUES("Well", "I\'m well.")')
-#    c.execute('INSERT INTO posts VALUES("Excellent", "I\'m excellent.")')
-#    c.execute('INSERT INTO posts VALUES("Okay", "I\'m okay.")')
-with sqlite3.connect('users.db') as connection:
+#with sqlite3.connect('users.db') as connection:
 
-    # get a cursor object used to execute SQL commands
-    c = connection.cursor()
+    ## get a cursor object used to execute SQL commands
+    #c = connection.cursor()
 
-    c.execute('DROP TABLE IF EXISTS users')
-    # create the table
-    c.execute('CREATE TABLE users(user TEXT, password TEXT)')
+    #c.execute('DROP TABLE IF EXISTS users')
+    ## create the table
+    #c.execute('CREATE TABLE users(user TEXT, password TEXT)')
 
-    # insert dummy data into the table
-    c.execute('INSERT INTO users VALUES("admin", "admin")')
-    c.execute('INSERT INTO users VALUES("dan", "dan")')
+    ## insert dummy data into the table
+    #c.execute('INSERT INTO users VALUES("admin", "admin")')
+    #c.execute('INSERT INTO users VALUES("dan", "dan")')
+
+import MySQLdb
+config = {
+    'user': 'root',
+    'passwd': '',
+    'host': 'localhost',
+    'db': 'test'
+}
+db = MySQLdb.connect(**config)
+cur = db.cursor()
+for line in open('test_setup.sql'):
+    cur.execute(line)
+db.commit()
