1e0aa3c69f49e54d552dd5edf0b1b8f1d665f469
Daniel Hill <daniel.hill@live.longwood.edu>
2015-04-22 10:22:30 -0400

    Due dates now have to be after assign dates.

--- login/static/default.js
+++ login/static/default.js
@@ -3,11 +3,13 @@ function validateFileUpload() {
     var due_date = document.forms["UploadForm"]["due_date"].value;
     var assign_date = document.forms["UploadForm"]["assign_date"].value;
     if (filename == null || filename == "") {
-        alert("Filename must be filled out.")
+        alert("Filename must be filled out.");
     } else if (due_date == null || due_date == "") {
-        alert("Due date must be filled out.")
+        alert("Due date must be filled out.");
     } else if (assign_date == null || assign_date == "") {
-        alert("Assign date must be filled out.")
+        alert("Assign date must be filled out.");
+    } else if (assign_date > due_date) {
+        alert("Assign date is after due date!");
     } else {
         return true;
     }
